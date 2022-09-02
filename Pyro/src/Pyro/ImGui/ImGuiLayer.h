778e4ce1d4f8582d1e5f473e729c259592dc5fbc
David Cataldo <dpcataldo06@gmail.com>
2022-09-02 17:34:41 -0400

    fixing imgui docking

--- Pyro/src/Pyro/ImGui/ImGuiLayer.h
+++ Pyro/src/Pyro/ImGui/ImGuiLayer.h
@@ -15,21 +15,9 @@ namespace Pyro
 		ImGuiLayer();
 		~ImGuiLayer();
 		
-		void OnAttach();
-		void OnDetach();
-		void OnUpdate();
-		void OnEvent(Event& evnt);
-
-	private:
-		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
-		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
-		bool OnMouseMovedEvent(MouseMovedEvent& e);
-		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
-		bool OnKeyPressedEvent(KeyPressedEvent& e);
-		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
-		bool OnKeyTypedEvent(KeyTypedEvent& e);
-		bool OnWindowResizeEvent(WindowResizeEvent& e);
-
+		virtual void OnAttach() override;
+		virtual void OnDetach() override;
+		virtual void OnUpdate() override;
 	private:
 		float m_Time = 0.0f;
 	};
