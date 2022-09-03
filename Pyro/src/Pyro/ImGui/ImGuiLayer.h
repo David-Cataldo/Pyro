6a77eccec54de55a49ba8d75d3f019471bae86a9
David Cataldo <dpcataldo06@gmail.com>
2022-09-02 23:58:01 -0400

    Revert "removed imgui submodule from .gitmodules"
    This reverts commit 749f0e8d38099c9b28cff93f7d12328163789e8c.

--- Pyro/src/Pyro/ImGui/ImGuiLayer.h
+++ Pyro/src/Pyro/ImGui/ImGuiLayer.h
@@ -15,9 +15,21 @@ namespace Pyro
 		ImGuiLayer();
 		~ImGuiLayer();
 		
-		virtual void OnAttach() override;
-		virtual void OnDetach() override;
-		virtual void OnUpdate() override;
+		void OnAttach();
+		void OnDetach();
+		void OnUpdate();
+		void OnEvent(Event& evnt);
+
+	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizeEvent(WindowResizeEvent& e);
+
 	private:
 		float m_Time = 0.0f;
 	};
