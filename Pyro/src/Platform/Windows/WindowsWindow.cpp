20e1256ccd7d97c4b1409d8a28f27e2c44f0539d
David Cataldo <dpcataldo06@gmail.com>
2022-09-16 15:13:24 -0400

    add openGL context class for rendering api

--- Pyro/src/Platform/Windows/WindowsWindow.cpp
+++ Pyro/src/Platform/Windows/WindowsWindow.cpp
@@ -5,7 +5,7 @@
 #include "Pyro/Events/MouseEvent.h"
 #include "Pyro/Events/KeyEvent.h"
 
-#include <glad/glad.h>
+#include "Platform/OpenGL/OpenGLContext.h"
 
 namespace Pyro
 {
@@ -49,10 +49,9 @@ namespace Pyro
 		}
 
 		m_Window = glfwCreateWindow((int)props.Width, (int)props.Height, m_Data.Title.c_str(), nullptr, nullptr);
-		glfwMakeContextCurrent(m_Window);
-
-		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
-		PY_CORE_ASSERT(status, "Failed to initialize Glad!");
+		
+		m_Context = new OpenGLContext(m_Window);
+		m_Context->Init();
 
 		glfwSetWindowUserPointer(m_Window, &m_Data);
 		SetVSync(true);
@@ -153,7 +152,7 @@ namespace Pyro
 	void WindowsWindow::OnUpdate()
 	{
 		glfwPollEvents();
-		glfwSwapBuffers(m_Window);
+		m_Context->SwapBuffers();
 	}
 
 	void WindowsWindow::SetVSync(bool enabled)
--- Pyro/src/Platform/Windows/WindowsWindow.h
+++ Pyro/src/Platform/Windows/WindowsWindow.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Pyro/Window.h"
+#include "Pyro/Renderer/GraphicsContext.h"
 
 #include <GLFW/glfw3.h>
 
@@ -28,6 +29,7 @@ namespace Pyro
 		virtual void Shutdown();
 	private:
 		GLFWwindow* m_Window;
+		GraphicsContext* m_Context;
 
 		struct WindowData
 		{
